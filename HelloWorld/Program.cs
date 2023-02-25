e2e9cbf8dd3163e63c92ad725f59376839215fb7
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-02-25 08:30:03 +0100

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Program
+{
+	/// <summary>
+	/// Playing around
+	/// </summary>
+	/// <param name="args"></param>
+	public static void Main(string[] args)
+	{
+		int age = GetAge(2, 1995);
+		Console.WriteLine("You're " + age + " years old");
+	}
+	/// <summary>
+	/// Calculates the age of a user
+	/// </summary>
+	/// <param name="month">Month of birth</param>
+	/// <param name="year">Year of Birth</param>
+	public static int GetAge(byte month, int year)
+	{
+		int thisYear = 2023;
+		int thisMonth = 2;
+		var age = (((thisYear * 12) + thisMonth) - (year * 12 + (month))) / 12;
+		return age;
+	}
+}
